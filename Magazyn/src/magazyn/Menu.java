f2cc8ab2e128a1f83221e88c03d6858eeebd2b22
Bartek1817 <Bartek1817@interia.pl>
2018-06-07 17:46:39 +0200

    pop-up test

--- Magazyn/src/magazyn/Menu.java
+++ Magazyn/src/magazyn/Menu.java
@@ -9,15 +9,22 @@
  */
 package magazyn;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -59,6 +66,20 @@ public abstract class Menu {
         ramka4.setFitHeight(35);
         ramka4.setFitWidth(125);
         ramka4.setVisible(false);
+
+        ImageView ramka5 = new ImageView("file:ramka.png");
+        ramka5.setLayoutX(270);
+        ramka5.setLayoutY(560);
+        ramka5.setFitHeight(35);
+        ramka5.setFitWidth(125);
+        ramka5.setVisible(false);
+
+        ImageView ramka6 = new ImageView("file:ramka.png");
+        ramka6.setLayoutX(420);
+        ramka6.setLayoutY(560);
+        ramka6.setFitHeight(35);
+        ramka6.setFitWidth(125);
+        ramka6.setVisible(false);
         ///////// TEXT
         Text Lista = new Text("Lista Zamówień");
         Lista.setStyle("-fx-font-size: 40pt;");
@@ -108,6 +129,22 @@ public abstract class Menu {
         Wylogujm.setLayoutY(500);
         Wylogujm.setLayoutX(30);
         Wylogujm.setPickOnBounds(true);
+
+        Text Dodaj = new Text("Dodaj Towar");
+        Dodaj.setStyle("-fx-font-size: 12pt;");
+        Dodaj.setFill(Color.WHITE);
+        Dodaj.setLayoutY(580);
+        Dodaj.setLayoutX(285);
+        Dodaj.setPickOnBounds(true);
+        Dodaj.setVisible(false);
+
+        Text Usuń = new Text("Usuń Towar");
+        Usuń.setStyle("-fx-font-size: 12pt;");
+        Usuń.setFill(Color.WHITE);
+        Usuń.setLayoutY(580);
+        Usuń.setLayoutX(435);
+        Usuń.setPickOnBounds(true);
+        Usuń.setVisible(false);
 
         Text cb = new Text("Aplication Speedy create by: AL && BŻ");
         cb.setStyle("-fx-font-size: 10pt;");
@@ -175,7 +212,91 @@ public abstract class Menu {
         vbox.setSpacing(5);
         vbox.setPadding(new Insets(10, 0, 0, 10));
         vbox.getChildren().add(tabela1);
+        ////////////////////////////////////////////
+        ///////////okno start
 
+        Rectangle r = new Rectangle(); // Kwadrat na reszte ekranu żeby zablokować inne klikanie
+        r.setX(0);
+        r.setY(0);
+        r.setWidth(1200);
+        r.setHeight(1200);
+        r.setArcWidth(1200);
+        r.setArcHeight(1200);
+        r.setFill(Color.TRANSPARENT);
+        r.setVisible(false);
+
+        Group oknoStart = new Group();
+        oknoStart.setVisible(false);
+
+        oknoStart.getChildren().add(new Rectangle(400, 300, new Color(1f, 1f, 1f, .85f)));
+        oknoStart.setLayoutX(300);
+        oknoStart.setLayoutY(190);
+
+        Label Tytul = new Label("Dodawanie Towaru");
+        Tytul.setLayoutX(150);
+        Tytul.setLayoutY(10);
+
+        Label NazwaTowarul = new Label("Nazwa Towaru");
+        NazwaTowarul.setLayoutX(10);
+        NazwaTowarul.setLayoutY(50);
+
+        TextField NazwaTowarut = new TextField();
+        NazwaTowarut.setText("Nazwa");
+        NazwaTowarut.setLayoutX(10);
+        NazwaTowarut.setLayoutY(80);
+
+        Label IlośćTowarul = new Label("Ilość Towaru");
+        IlośćTowarul.setLayoutX(10);
+        IlośćTowarul.setLayoutY(120);
+
+        TextField IlośćTowarut = new TextField();
+        IlośćTowarut.setText("Ilość");
+        IlośćTowarut.setLayoutX(10);
+        IlośćTowarut.setLayoutY(150);
+
+        Label RegałL = new Label("Regał");
+        RegałL.setLayoutX(10);
+        RegałL.setLayoutY(190);
+
+        TextField RegałT = new TextField();
+        RegałT.setText("Regał");
+        RegałT.setLayoutX(10);
+        RegałT.setLayoutY(220);
+
+        Button btn000 = new Button();
+        btn000.setText("X");
+        btn000.setLayoutX(370);
+        btn000.setLayoutY(10);
+        btn000.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                oknoStart.setVisible(false);
+                 r.setVisible(false);
+            }
+        });
+
+        Button btn001 = new Button();
+        btn001.setText("Dodaj Towar");
+        btn001.setLayoutX(10);
+        btn001.setLayoutY(265);
+        btn001.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.print("dodano i chuj");
+            }
+        });
+
+        oknoStart.getChildren().add(Tytul);
+        oknoStart.getChildren().add(NazwaTowarul);
+        oknoStart.getChildren().add(NazwaTowarut);
+        oknoStart.getChildren().add(IlośćTowarul);
+        oknoStart.getChildren().add(IlośćTowarut);
+        oknoStart.getChildren().add(RegałL);
+        oknoStart.getChildren().add(RegałT);
+        oknoStart.getChildren().add(btn000);
+        oknoStart.getChildren().add(btn001);
+
+        ///////////koniec = okno start
         //////////////////////////////////////////// 
         root.getChildren().add(Lista);
         root.getChildren().add(Towary);
@@ -184,19 +305,31 @@ public abstract class Menu {
         root.getChildren().add(Towarym);
         root.getChildren().add(Infom);
         root.getChildren().add(Wylogujm);
+
         root.getChildren().add(cb);
         root.getChildren().add(logo);
         root.getChildren().add(ramka1);
         root.getChildren().add(ramka2);
         root.getChildren().add(ramka3);
         root.getChildren().add(ramka4);
+
         root.getChildren().add(vbox);
+        
+        root.getChildren().add(Dodaj);
+        root.getChildren().add(Usuń);
+        root.getChildren().add(ramka5);
+        root.getChildren().add(ramka6);
+        root.getChildren().add(r);
+        
+       root.getChildren().add(oknoStart);
         //////// WYKONANIE
 
         Listam.setOnMouseClicked((MouseEvent e) -> { // Po kliknieciu wykonaj
             Info.setVisible(false);
             Towary.setVisible(false);
             Lista.setVisible(true);
+            Usuń.setVisible(false);
+            Dodaj.setVisible(false);
             vbox.getChildren().clear();
             vbox.getChildren().add(tabela1);
 
@@ -214,6 +347,8 @@ public abstract class Menu {
             Info.setVisible(false);
             Towary.setVisible(true);
             Lista.setVisible(false);
+            Usuń.setVisible(true);
+            Dodaj.setVisible(true);
             vbox.getChildren().clear();
             vbox.getChildren().add(tabela3);
         });
@@ -231,6 +366,8 @@ public abstract class Menu {
             Info.setVisible(true);
             Towary.setVisible(false);
             Lista.setVisible(false);
+            Usuń.setVisible(false);
+            Dodaj.setVisible(false);
             vbox.getChildren().clear();
             vbox.getChildren().add(tabela2);
         });
@@ -240,6 +377,29 @@ public abstract class Menu {
         });
         Infom.setOnMouseEntered((MouseEvent e) -> { // Po najechaniu wykonaj
             ramka3.setVisible(true);
+
+        });
+        Dodaj.setOnMouseClicked((MouseEvent e) -> { // Po kliknieciu wykonaj
+            oknoStart.setVisible(true);
+             r.setVisible(true);
+        });
+        Dodaj.setOnMouseExited((MouseEvent e) -> { // Po zjechaniu wykonaj
+            ramka5.setVisible(false);
+
+        });
+        Dodaj.setOnMouseEntered((MouseEvent e) -> { // Po najechaniu wykonaj
+            ramka5.setVisible(true);
+
+        });
+        Usuń.setOnMouseClicked((MouseEvent e) -> { // Po kliknieciu wykonaj
+            oknoStart.setVisible(true);
+        });
+        Usuń.setOnMouseExited((MouseEvent e) -> { // Po zjechaniu wykonaj
+            ramka6.setVisible(false);
+
+        });
+        Usuń.setOnMouseEntered((MouseEvent e) -> { // Po najechaniu wykonaj
+            ramka6.setVisible(true);
 
         });
 
